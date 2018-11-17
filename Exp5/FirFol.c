2072c6163238825faafdf3bc106cba498dd620ec
Joseph A <joeavaikath@gmail.com>
2018-11-17 14:17:23 +0530

    ICG Added

--- Exp5/FirFol.c
+++ Exp5/FirFol.c
@@ -3,34 +3,56 @@
 #include<ctype.h>
 
 char rules[6][10];
-char first[6][5];
-int n, i = 0;
+char first[10][10], follow[10][10];
+int m = 0, n = 0, count = 6;
 
-void first(int rno){
+void first(char c){
 	
-	if(rules[rno][2]>a && rules[rno][2]<z){
-	
-		first[rno][i] = rules[rno][2];
-	}
-	else{
+	for(int rno=0;rno<count;rno++){
 		
-		firs
+		if(c == rules[rno][2]){	
+			
+			m = 0;	
+			if(rules[rno][2]>a && rules[rno][2]<z){
+	
+				first[rno][m++] = rules[rno][2];
+			}
+			else{
+		
+				first(rules[rno][2]);
+			}
+		}
 	}
 }
 
+void pFirst(){
+
+	for(int i=0;i<count;i++){
+
+		
+
 void main(){
 	
+	/*
 	printf("\n Enter no. of productions: ");
 	scanf("%d", n);
 	
+	for(int i = 0;i < count;i++){
+	
+		printf("\n Enter rule:");
+		scanf("%s", &rules[i]):
 
-		strcpy(rules[0], "S=AB");
-		strcpy(rules[1], "S=cd");
-		strcpy(rules[2], "B=cAd");
-		strcpy(rules[3], "B=b");
-		strcpy(rules[4], "A=a");
-		strcpy(rules[5], "A=b");
+	}
+	*/
 
+	strcpy(rules[0], "S=AB");
+	strcpy(rules[1], "S=cd");
+	strcpy(rules[2], "B=cAd");
+	strcpy(rules[3], "B=b");
+	strcpy(rules[4], "A=a");
+	strcpy(rules[5], "A=b");
+
+	
 
 		
 }
---
+++ Exp7/ICG.c
@@ -0,0 +1,103 @@
+#include<stdio.h>
+#include<string.h>
+
+char string[10];
+int i, j, flag;
+
+void push(char st[], char c, int *top){
+
+	*top = *top + 1;
+	st[*top] = c;
+
+}
+
+char pop(char st[], int *top){
+
+	int temp = *top;
+	*top = *top - 1;
+	return st[temp];
+
+}
+
+int getP(char c){
+
+	switch(c){
+
+		case '+': return 2;
+			  break;
+		case '-':return 2;
+				break;
+		case '*':return 3;
+			break;
+		case '/':return 3;
+			break;
+		case '$':return 0;
+			break;
+		case '=':return 1;
+			break;
+		}
+} 
+
+
+void main(){
+
+	printf("\n Enter the IP string: ");
+	scanf("%s", string);
+
+	//stack 1 for operands. stack 2 for operators. t for tenmperory variables
+	char stack1[10], stack2[10], t[10][10]; 
+	int top1, top2, choice, itr = 2, titr = 0, Ti = 0;
+	top1=top2=-1;
+	
+	push(stack1, string[0], &top1);
+	push(stack2, string[1], &top2);
+	
+	printf("\n Operator Op1 Op2 NewOP");
+
+	while(top1!=-1 && itr<strlen(string)){
+
+		//printf("\nitr :%d top1:%d top2:%d", itr, top1, top2);
+
+		if(string[itr] == '+' || 
+		   string[itr] == '-' || 
+		   string[itr] == '*' || 
+		   string[itr] == '/' ||
+		   string[itr] == '$'){
+
+			
+			//printf("\nStack:%c IP STring:%c", stack2[top2], string[itr]);		
+			while(getP(stack2[top2]) >= getP(string[itr])){
+
+				t[titr][2] = pop(stack1, &top1);
+				t[titr][0] = pop(stack1, &top1);
+				push(stack1, Ti++ + '0', &top1);
+				t[titr][1] = pop(stack2, &top2);
+				t[titr][3] = '\0';
+
+				printf("\n %c         %c    %c    %c", t[titr][1], t[titr][0], t[titr][2], Ti-1+'0');
+				titr++;
+				//printf("In popping");
+							
+			}
+			
+		
+			push(stack2, string[itr], &top2);
+		
+
+
+			
+		}
+		else{
+			push(stack1, string[itr], &top1);
+		}
+		itr++;
+	
+	
+	}
+	printf("\n\n\n t values: \n");	
+	for(i=0;i<titr;i++)
+		printf("\nt%d   %s", i, t[i]);
+	
+	
+	
+}
